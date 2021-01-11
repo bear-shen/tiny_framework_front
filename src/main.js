136d16b236f5f3e565f2fd377555bc4eea296d71
shen <bear_shen@live.cn>
2021-01-12 02:43:49 +0800

    kill bootstrap

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import router from './router'
 import store  from './store'
 
 //教程说是要导入，但是写进去反而会导致没有导入，没有确认具体的原因，反正先扔掉算了
-import 'bootstrap/dist/js/bootstrap.min'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './assets/darkstrap.css'
+// import 'bootstrap/dist/js/bootstrap.min'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import './assets/darkstrap.css'
 import './assets/fonts/sysIcon/iconfont.css'
 import './assets/fonts/listIcon/iconfont.css'
 
